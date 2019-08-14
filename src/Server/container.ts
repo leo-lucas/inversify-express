7c11f3407c9782f5adf44c5b45bbafdf0d790944
Leonardo Lucas dos Santos <leonardo.santos@meta.com.br>
2019-08-13 21:50:00 -0300

    ajuste de exemplo

--- src/Server/container.ts
+++ src/Server/container.ts
@@ -1,11 +1,9 @@
 import { Container } from 'inversify'
 import { TestController, ITestController } from '../Controllers/TestController'
-import { TestControllerTwo } from '../Controllers/TestControllerTwo'
 // set up container
 let container = new Container()
 
 // set up bindings
 container.bind<ITestController>('TestController').to(TestController)
-container.bind<TestControllerTwo>('TestControllerTwo').to(TestControllerTwo)
 
 export default container
