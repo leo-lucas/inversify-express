a48d26364e580264684080afc28a0ff9d211a6af
Leonardo Lucas dos Santos <leonardo.santos@meta.com.br>
2019-08-15 00:10:37 -0300

    removido o build

--- build/Core/TypeOperacoes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TypeOperacoes;
-(function (TypeOperacoes) {
-    TypeOperacoes["add"] = "add";
-    TypeOperacoes["sub"] = "sub";
-    TypeOperacoes["mul"] = "mul";
-    TypeOperacoes["div"] = "div";
-})(TypeOperacoes = exports.TypeOperacoes || (exports.TypeOperacoes = {}));
